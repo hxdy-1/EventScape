5dbe7af897857add904faa6da0d33d8e1871e2a8
hady <hadimomin02@gmail.com>
2024-01-19 20:13:37 +0530

    updating styles...

--- frontend/src/components/EventsList.js
+++ frontend/src/components/EventsList.js
@@ -1,29 +1,30 @@
 // import { useLoaderData } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import classes from './EventsList.module.css';
+import classes from "./EventsList.module.css";
+import { motion } from "framer-motion";
 
-function EventsList({events}) {
-  // const events = useLoaderData();
+function EventsList({ events }) {
+	// const events = useLoaderData();
 
-  return (
-    <div className={classes.events}>
-      <h1>All Events</h1>
-      <ul className={classes.list}>
-        {events.map((event) => (
-          <li key={event.id} className={classes.item}>
-            <Link to={`/events/${event.id}`}>
-              <img src={event.image} alt={event.title} />
-              <div className={classes.content}>
-                <h2>{event.title}</h2>
-                <time>{event.date}</time>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+	return (
+		<div className={classes.events}>
+			<h1>All Events</h1>
+			<ul className={classes.list}>
+				{events.map((event) => (
+					<li key={event.id} className={classes.item}>
+						<Link to={`/events/${event.id}`}>
+							<img src={event.image} alt={event.title} />
+							<div className={classes.content}>
+								<h2>{event.title}</h2>
+								<time>{event.date}</time>
+							</div>
+						</Link>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default EventsList;
