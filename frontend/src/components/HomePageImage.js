c2723897ee7aa7b31d380928a3705414fea9fafc
hady <hadimomin02@gmail.com>
2024-01-19 20:31:08 +0530

    navigating from image to Events

--- frontend/src/components/HomePageImage.js
+++ frontend/src/components/HomePageImage.js
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
 import image from "../assets/booking_schedule_isometric_illustration-05-removebg-preview.png";
 import classes from "./HomePageImage.module.css";
 
 const HomePageImage = () => {
+	const navigate = useNavigate();
 	return (
 		<div className={classes.container}>
-			<img src={image} alt="events image"></img>
+			<img
+				src={image}
+				alt="events image"
+				onClick={() => navigate("/events")}
+			></img>
 		</div>
 	);
 };
