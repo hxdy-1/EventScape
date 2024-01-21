c601c5ad6310afce2e679569892d29162e414f6f
hady <hadimomin02@gmail.com>
2024-01-21 14:20:44 +0530

    update data

--- backend/data/event.js
+++ backend/data/event.js
@@ -1,56 +1,60 @@
-const { v4: generateId } = require('uuid');
+const { NotFoundError } = require("../util/errors");
+const { Events } = require("../DB/db");
 
-const { NotFoundError } = require('../util/errors');
-const { readData, writeData } = require('./util');
-
+// Retrieving all events:
 async function getAll() {
-  const storedData = await readData();
-  if (!storedData.events) {
-    throw new NotFoundError('Could not find any events.');
-  }
-  return storedData.events;
+	const storedData = await Events.find();
+	// console.log("All events: ", storedData);
+
+	if (!storedData || storedData.length === 0) {
+		throw new NotFoundError("Could not find any Events.");
+	}
+	return storedData;
 }
 
+// Retrieving an event by its _id:
 async function get(id) {
-  const storedData = await readData();
-  if (!storedData.events || storedData.events.length === 0) {
-    throw new NotFoundError('Could not find any events.');
-  }
+	// console.log("id from get: ", id);
+	const event = await Events.findOne({ _id: id });
 
-  const event = storedData.events.find((ev) => ev.id === id);
-  if (!event) {
-    throw new NotFoundError('Could not find event for id ' + id);
-  }
-
-  return event;
+	// console.log("single event: ", event);
+	if (!event) {
+		throw new NotFoundError("Could not find event for id " + id);
+	}
+	return event;
 }
 
+// Adding a new event:
 async function add(data) {
-  const storedData = await readData();
-  storedData.events.unshift({ ...data, id: generateId() });
-  await writeData(storedData);
+	// Creating a new instance/document of the Events model
+	// console.log("data from add: ", data);
+	const newEvent = new Events({ ...data });
+
+	// console.log("newly added event: ", newEvent);
+	await newEvent.save();
 }
 
+// Replace an existing event by its ID
 async function replace(id, data) {
-  const storedData = await readData();
-  if (!storedData.events || storedData.events.length === 0) {
-    throw new NotFoundError('Could not find any events.');
-  }
+	// console.log("id from replace: ", id);
+	// console.log("data from replace: ", data);
+	const event = await Events.findByIdAndUpdate(
+		id,
+		{ ...data, id },
+		{ new: true, runValidators: true }
+	);
 
-  const index = storedData.events.findIndex((ev) => ev.id === id);
-  if (index < 0) {
-    throw new NotFoundError('Could not find event for id ' + id);
-  }
-
-  storedData.events[index] = { ...data, id };
-
-  await writeData(storedData);
+	// Throw an error if the event is not found
+	if (!event) {
+		throw new NotFoundError("Could not find event for id " + id);
+	}
 }
 
+// Remove an event by its ID
 async function remove(id) {
-  const storedData = await readData();
-  const updatedData = storedData.events.filter((ev) => ev.id !== id);
-  await writeData({ ...storedData, events: updatedData });
+	// await Events.deleteOne({ id });
+	console.log("id from remove: ", id);
+	await Events.findByIdAndDelete(id);
 }
 
 exports.getAll = getAll;
--- backend/data/user.js
+++ backend/data/user.js
@@ -1,33 +1,26 @@
-const { hash } = require('bcryptjs');
-const { v4: generateId } = require('uuid');
-
-const { NotFoundError } = require('../util/errors');
-const { readData, writeData } = require('./util');
+const { hash } = require("bcryptjs");
+const { NotFoundError } = require("../util/errors");
+const { Users } = require("../DB/db");
 
 async function add(data) {
-  const storedData = await readData();
-  const userId = generateId();
-  const hashedPw = await hash(data.password, 12);
-  if (!storedData.users) {
-    storedData.users = [];
-  }
-  storedData.users.push({ ...data, password: hashedPw, id: userId });
-  await writeData(storedData);
-  return { id: userId, email: data.email };
+	const hashedPw = await hash(data.password, 12);
+
+	// Create a new user instance/document using the Users model:
+	const newUser = new Users({ ...data, password: hashedPw });
+	await newUser.save();
+
+	return { id: newUser.id, email: newUser.email };
 }
 
 async function get(email) {
-  const storedData = await readData();
-  if (!storedData.users || storedData.users.length === 0) {
-    throw new NotFoundError('Could not find any users.');
-  }
+	// Using the findOne method on the Users model to find a user by email
+	const user = await Users.findOne({ email });
 
-  const user = storedData.users.find((ev) => ev.email === email);
-  if (!user) {
-    throw new NotFoundError('Could not find user for email ' + email);
-  }
+	if (!user) {
+		throw new NotFoundError("Could not find user for email " + email);
+	}
 
-  return user;
+	return user;
 }
 
 exports.add = add;
