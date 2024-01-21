94b818c7fe513ea51c491e21b9f61f58801f4e30
hady <hadimomin02@gmail.com>
2024-01-21 14:26:41 +0530

    update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,34 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+const bodyParser = require("body-parser");
+const express = require("express");
 
-const eventRoutes = require('./routes/events');
-const authRoutes = require('./routes/auth');
+const eventRoutes = require("./routes/events");
+const authRoutes = require("./routes/auth");
+
+require("dotenv").config();
+const backend_port = process.env.backend_port;
+
+console.log("backend_port", backend_port);
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  next();
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+	next();
 });
 
 app.use(authRoutes);
 
-app.use('/events', eventRoutes);
+app.use("/events", eventRoutes);
 
 app.use((error, req, res, next) => {
-  const status = error.status || 500;
-  const message = error.message || 'Something went wrong.';
-  res.status(status).json({ message: message });
+	const status = error.status || 500;
+	const message = error.message || "Something went wrong.";
+	res.status(status).json({ message: message });
 });
 
-app.listen(8080);
+app.listen(backend_port || 8080, () =>
+	console.log(`Server is listening on ${backend_port || 8080}`)
+);
