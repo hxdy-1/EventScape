14579db110473259efe610a1dedb3a6ca77218e1
hady <hadimomin02@gmail.com>
2024-01-21 14:53:29 +0530

    update EventItem.js

--- frontend/src/components/EventItem.js
+++ frontend/src/components/EventItem.js
@@ -2,6 +2,15 @@ import { Link, useRouteLoaderData, useSubmit } from "react-router-dom";
 
 import classes from "./EventItem.module.css";
 
+function formatDate(dateString) {
+	const options = { day: "2-digit", month: "2-digit", year: "numeric" };
+	const [day, month, year] = new Date(dateString)
+		.toLocaleDateString("en-GB", options)
+		.split("/");
+
+	return `${day}-${month}-${year}`;
+}
+
 function EventItem({ event }) {
 	const token = useRouteLoaderData("root");
 	const submit = useSubmit();
@@ -18,7 +27,8 @@ function EventItem({ event }) {
 		<article className={classes.event}>
 			<img src={event.image} alt={event.title} />
 			<h1>{event.title}</h1>
-			<time>{event.date}</time>
+			{/* <time>{event.date}</time> */}
+			<time>{formatDate(event.date)}</time>
 			<p>{event.description}</p>
 			{token && (
 				<menu className={classes.actions}>
